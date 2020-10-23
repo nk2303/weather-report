5d9818237aa5b2ffc782e066ebffce5c5fdf599e
nk2303 <ngankhong03@gmail.com>
2020-10-22 21:43:28 -0700

    need converting city name to geolocation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import weather from 'weather-gov-api'
+import UsCities from './UsCities';
 
 function App() {
+
+  const [selectCity, setSelectCity] = useState("Choose city")
+
+  const getWeather = (lat, long) => {
+    weather.getForecast('default', 40.71, -74.01)
+      .then(data => { console.log(data.data.properties.periods[0])})
+      .catch(err => console.log(err))
+  }
+  getWeather()
+  const getCoordinates = city => {
+    console.log(city)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    getCoordinates(selectCity)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Simple Weather App</h1>
+        <form onSubmit={handleSubmit}>
+        <label>
+          PICK LOCATION
+        </label>
+        <select value={selectCity} onChange={e => setSelectCity(e.target.value)}>
+            {UsCities.map(city => <option key={city} value={city}>{city}</option>)}
+          </select>
+        <input type="submit" value="Submit" />
+      </form>
+        <p>WEATHER</p>
       </header>
     </div>
   );
